74b87ba5c79859e2a8e01066b86abab0847da2e7
trunyashinainecca <ineccaa271891@gmail.com>
2026-07-02 20:11:17 +0400

    new file fixture

--- test_api.py
+++ test_api.py
@@ -1,11 +1,5 @@
-import pytest
 import requests
 
-@pytest.fixture #нужно чтобы вынести общий код в одно место #специальная функция подготовкиа
-def users(): # назывние фикстуры.Потом ее будем исполтьзовать users прямо в текте
-    response = requests.get(URL, timeout=5)
-    assert response.status_code == 200
-    return response.json() # фикстур возращает список пользователей
 
 URL = "https://jsonplaceholder.typicode.com/users"
 
