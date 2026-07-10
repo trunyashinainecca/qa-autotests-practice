d5041f6a2c670c9a443453f1964c2a7f2e62f2ec
trunyashinainecca <ineccaa271891@gmail.com>
2026-07-10 22:09:46 +0400

    add test api crud

--- test_posts.py
+++ test_posts.py
@@ -89,4 +89,72 @@ def test_get_non_existing_post_returns_404():
     assert body == {}
 
 
+def test_create_posts():
+    new_post={
+        "userId":1,
+        "title":"Test title",
+        "body":"Test body"
+    }
+    response = requests.post(URL,json=new_post, timeout=10) #отправить пост запрос на адрес постс и передай тело запроса в нью пост в формат json
+    assert response.status_code == 201
+
+    create_post = response.json()
+
+    assert isinstance(create_post,dict)
+    assert create_post["userId"] == new_post["userId"]
+    assert create_post["title"] == new_post["title"]
+    assert create_post["body"] == new_post["body"]
+    assert "id" in create_post
+    assert isinstance(create_post["id"], int) # если объект создан то появится его айди
+
+def test_create_post_with_another_data():
+    new_post={
+        "userId":2,
+        "title":"Test love text",
+        "body":"Test body love text"
+    }
+    response = requests.post(URL, json=new_post, timeout=5)
+
+    assert response.status_code == 201
+
+    create_post = response.json()
+
+    assert isinstance(create_post, dict) #так проверяем что среатпост у нас словарь
+    assert create_post["userId"] == new_post["userId"]
+    assert create_post["title"] == new_post["title"]
+    assert create_post["body"] == new_post["body"]
+    assert "id" in create_post
+    assert isinstance(create_post["id"], int)
+
+def test_update_post():
+    updated_post = {
+        "userId": 1,
+        "id": 1,
+        "title": "update title",
+        "body": "update body"
+        }
+    response = requests.put(f"{URL}/1", json=updated_post, timeout=5)
+
+    assert response.status_code == 200
+
+    post = response.json()
     
+    assert isinstance(post, dict)
+    assert updated_post["userId"] == post["userId"]
+    assert updated_post["id"] == post["id"]
+    assert updated_post["title"] == post["title"]
+    assert updated_post["body"] == post["body"]
+
+def test_patch_post_title():
+    patched_data={"title":"Test love r"}
+    response = requests.patch(f"{URL}/1", json=patched_data, timeout=5)
+    assert response.status_code == 200
+    post = response.json()
+    assert isinstance(post, dict)
+    assert post["title"] == patched_data["title"]
+
+def test_delete_post():
+     response = requests.delete(f"{URL}/1", timeout=5) #Отправляем запрос на удаление поста с id = 1
+     assert response.status_code == 200
+     body = response.json()
+     assert body == {}
